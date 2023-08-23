52e3e3277ef6afb64c6a83cebbccea0a7fc38219
Adler Luders <adlerluders@gmail.com>
2023-08-22 21:12:20 -0400

    added pathname hook

--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/nav.module.css";
@@ -6,20 +7,27 @@ import linkIcon from "../public/images/icon-link.svg";
 import profileIcon from "../public/images/icon-profile-details-header.svg";
 import Image from "next/image";
 import Button from "./Button";
+import { usePathname } from "next/navigation";
 
 function Nav() {
-  const { nav, links, linkItem } = styles;
+  const pathname = usePathname();
+
+  const { nav, links, linkItem, active } = styles;
   return (
     <nav className={nav}>
       <Image src={logo} alt="devlinks logo" />
 
       <div className={links}>
-        <div className={linkItem}>
+        <div
+          className={`${linkItem} ${pathname === "/account/links" && active}`}
+        >
           <Image src={linkIcon} alt="link icon" />
           <Link href={"/account/links"}>Links</Link>
         </div>
 
-        <div className={linkItem}>
+        <div
+          className={`${linkItem} ${pathname === "/account/details" && active}`}
+        >
           <Image src={profileIcon} alt="profile icon" />
           <Link href={"/account/details"}>Profile Details</Link>
         </div>
